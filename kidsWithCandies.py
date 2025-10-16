bb5ec23af2971d2e3a976b8d3e8604f22dc407fc
Paul Harder <pauljharder@hotmail.com>
2025-10-16 12:34:14 -0600

    fixed

--- kidsWithCandies.py
+++ kidsWithCandies.py
@@ -1,7 +1,10 @@
-def kidsWithCandies(self, candies, extraCandies):
-        """
-        :type candies: List[int]
-        :type extraCandies: int
-        :rtype: List[bool]
-        """
+def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        result = []
         x = max(candies)
+        for c in candies:
+                if ((c + extraCandies) >= x):
+                        result.append(True)
+                else:
+                        result.append(False)
+
+        return result
